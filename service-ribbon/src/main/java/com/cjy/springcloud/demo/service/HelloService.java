54ba9c8ea0be6a992381a3ee1a41475e681ee5c8
w13731199 <407253741@qq.com>
2018-08-22 17:09:26 +0800

    add hystrix

--- service-ribbon/src/main/java/com/cjy/springcloud/demo/service/HelloService.java
+++ service-ribbon/src/main/java/com/cjy/springcloud/demo/service/HelloService.java
@@ -1,5 +1,6 @@
 package com.cjy.springcloud.demo.service;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -10,10 +11,15 @@ public class HelloService {
 	@Autowired
 	RestTemplate restTemplate;
 
+	@HystrixCommand(fallbackMethod = "hiError")
 	public String hiService(String name) {
 
 		return restTemplate.getForObject("http://eureka-hi/hi?name=".concat(name), String.class);
 
+	}
+
+	public String hiError(String name) {
+		return "hi,".concat(name).concat(",sorry,error");
 	}
 
 }
